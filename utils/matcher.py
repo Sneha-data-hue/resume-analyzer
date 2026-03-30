6ff89c3aad9c366b3a94aff7c1862ac33cdd2b58
Sneha-data-hue <innusne14@gmail.com>
2026-03-30 21:20:19 +0530

    Resolved all merge conflicts

--- utils/matcher.py
+++ utils/matcher.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 def calculate_match(resume_skills, job_skills):
     matched = list(set(resume_skills) & set(job_skills))
     missing = list(set(job_skills) - set(resume_skills))
@@ -8,15 +7,4 @@ def calculate_match(resume_skills, job_skills):
     else:
         score = (len(matched) / len(job_skills)) * 100
 
-=======
-def calculate_match(resume_skills, job_skills):
-    matched = list(set(resume_skills) & set(job_skills))
-    missing = list(set(job_skills) - set(resume_skills))
-
-    if len(job_skills) == 0:
-        score = 0
-    else:
-        score = (len(matched) / len(job_skills)) * 100
-
->>>>>>> 5b96f95224f9c5fe4c27254088a6331e04a3a0be
     return round(score, 2), matched, missing
