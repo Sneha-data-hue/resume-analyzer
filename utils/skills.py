b9a2e3043f655c065b553ea9b5d1ae5a374c92e7
Sneha-data-hue <innusne14@gmail.com>
2026-03-30 21:19:10 +0530

    Fixed skills merge conflict

--- utils/skills.py
+++ utils/skills.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 def extract_skills(text, job_skills):
     found_skills = []
     text = text.lower()
@@ -7,14 +6,4 @@ def extract_skills(text, job_skills):
         if skill.lower() in text:
             found_skills.append(skill)
 
-=======
-def extract_skills(text, job_skills):
-    found_skills = []
-    text = text.lower()
-
-    for skill in job_skills:
-        if skill.lower() in text:
-            found_skills.append(skill)
-
->>>>>>> 5b96f95224f9c5fe4c27254088a6331e04a3a0be
     return list(set(found_skills))
